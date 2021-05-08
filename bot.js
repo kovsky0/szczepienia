c4fd0092ed698d869a9cfe97b08cbe2eddfbc05e
Dariusz Jargus <darekjargus@gmail.com>
2021-05-08 15:56:43 +0200

    added confirm date

--- bot.js
+++ bot.js
@@ -1,26 +1,33 @@
-let getSelectButton = () => {
-  const x = [...document.querySelectorAll("button")].filter(b =>
-    b.textContent.includes("Wybierz")
+const getButton = (text) => {
+  const x = [...document.querySelectorAll("button")].filter((b) =>
+    b.textContent.includes(text)
   );
   return x.length && x[0];
 };
-let ding = () => {
+const ding = () => {
   const a = new Audio(
     "https://freesound.org/data/previews/387/387533_3829977-lq.mp3"
   );
   a.play();
 };
-let iterate = () => {
-  document.querySelector('button[type=submit]').click();
+const confirmDate = () => {
+  setTimeout(() => {
+    const confirmButton = getButton("Tak");
+    confirmButton && confirmButton.click();
+  }, 1500);
+};
+const iterate = () => {
+  document.querySelector("button[type=submit]").click();
   const process = () => {
     setTimeout(() => {
       const loader = document.querySelector(".sc-gsTCUz > .sc-dQppl");
       if (loader) {
         process();
       } else {
-        const selectButton = getSelectButton();
+        const selectButton = getButton("Wybierz");
         if (selectButton) {
           selectButton.click();
+          confirmDate();
           ding();
         } else {
           setTimeout(() => iterate(), 2500);
